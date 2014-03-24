b7f8c6007415094cf7940935911d5d445331ef2a
Samantha Rae Rack <srack@student02.cse.nd.edu>
2014-03-24 15:21:00 -0400

    StaticObj, Platform, and Ladder classes added.  Needed Platform objects are all initialized, and blit to the screen correctly

--- src/CApp.h
+++ src/CApp.h
@@ -12,10 +12,14 @@
 #include "CSurface.h"
 #include "CAnimation.h"
 #include "CEntity.h"
+#include "StaticObj.h"
+#include "Ladder.h"	/////////
+#include "Platform.h"	/////////
 
 #include "Define.h"
 
 #include <iostream>
+#include <vector>
 using namespace std ;
 
 class CApp : public CEvent {	//inherits from CEvent class
@@ -31,18 +35,24 @@ class CApp : public CEvent {	//inherits from CEvent class
 		void OnRender() ;
 		void OnCleanup() ;
 
-
-
 	private :
+		int OnInit_Static() ;	//helper function to initialize all static data members
+
 		int running ;	//condition for while loop
 		SDL_Surface* Surf_Display ;	//"blank piece of paper"
-		SDL_Surface* Surf_Background ;
+		SDL_Surface* Surf_bgObjs ;
 
-		TTF_Font* font ;
-		SDL_Surface* Surf_Highscore ;
-		SDL_Color textColor ;	
 
-		CEntity marioEntity ;
+		vector< Platform > platforms ;	//vector to hold all instantiated Platform objects
+		vector< Ladder > ladders  ;	//vector to hold all ladder objects
+
+		vector< StaticObj*> bgObjs ;	//vector of pointers to all static "background" objects (Ladders and Platforms), used to invoke polymorphic behavior
+
+//		TTF_Font* font ;
+//		SDL_Surface* Surf_Highscore ;
+//		SDL_Color textColor ;	
+
+//		CEntity marioEntity ;
 
 } ;
 
--- src/CApp_OnInit.cpp
+++ src/CApp_OnInit.cpp
@@ -8,31 +8,59 @@
 
 int CApp::OnInit() {
 	if(SDL_Init(SDL_INIT_EVERYTHING) < 0) {	//sets up SDL environment
+		cout << "SDL_Init fail" << endl ;
 		return 0 ;	//if set up failed
 	}
 	
 	Surf_Display = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, 32, SDL_HWSURFACE | SDL_DOUBLEBUF) ;	
 	if(Surf_Display == NULL) {
+		cout << "Window create fail" << endl ;
 		return 0 ;	//if window creation failed
 	}		
 
 	SDL_WM_SetCaption("Donkey Kong", NULL) ;	//window caption
 
-	Surf_Background = CSurface::OnLoad("pngFiles/bgFiles/_dkL1bg_7.png") ;
-	if(Surf_Background== NULL) return 0 ;
+////////////////
 
 
-	if(marioEntity.OnLoad("pngFiles/marioWalking.png", 20, 40, 8) == 0) return 0 ;
+	if(!OnInit_Static()) {
+		cout << "OnInit_Static() fail" << endl ;
+		return 0 ;
+	}
+
+/*	int i ;
+	Ladder temp ;
+	for( i = 0 ; i < 10 ; i++ ) {	
+		temp.OnInit(i*30, i*35, LADDER_WIDTH, 20+i*10) ;
+		ladders.push_back(temp) ;
+	}
+
+	p1.OnInit(60, 400, PLATFORM_WIDTH, PLATFORM_HEIGHT) ;
+*/
+
+
+//////////////
+/*	if(marioEntity.OnLoad("pngFiles/marioWalking.png", 20, 40, 8) == 0) {
+		cout << "Mario didn't load" << endl ; 
+		return 0 ;
+	}
 
 	marioEntity.setX(20) ;
 	marioEntity.setY(WINDOW_HEIGHT-60) ;
 
 	CEntity::entityList.push_back(&marioEntity) ;
+*/
 
-	if(TTF_Init() < 0) return 0 ;
+/*	if(TTF_Init() < 0){
+		 cout << "TTF_Init() fail" << endl ; 
+		return 0 ;
+	}
 	font = TTF_OpenFont("lazy.ttf", 20) ;
-	if(font == NULL) return 0 ;
+	if(font == NULL) {
+		cout << "Font did not open" << endl ; 
+		return 0 ;
+	}
 	Surf_Highscore = TTF_RenderText_Solid(font, "HIGHSCORE", textColor);
-
+*/
 	return 1 ;	//true
 }
